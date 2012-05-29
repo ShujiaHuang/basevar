3beb77760ecbb9ae80198a62b15f6f153b41fbf4
Shujia Huang <huangshujia9@gmail.com>
2012-05-29 18:16:00 +0800

    compile the RE patten before use it

--- basevar/mpileup.py
+++ basevar/mpileup.py
@@ -8,6 +8,9 @@ import numpy as np
 
 import utils
 
+REOBJ_RM_START_END = re.compile('\^\S|\$')
+REOBJ_RM_INDEL = re.compile('[-+]\d+[ACGTacgtNn]+')
+
 def rmStartEnd(bases):
     """
     remove start(`^`) and end(`$`) character
@@ -19,10 +22,11 @@ def rmStartEnd(bases):
 
     >>> import mpileup
     >>> bases="...,$.$.$A,..A...,,,.,,...+5AGGC...-8GTCGG......,a,^F,^].^F,"
-    >>> mpileup.clip(bases)
+    >>> mpileup.rmStartEnd(bases)
     ... ...,..A,..A...,,,.,,...+5AGGC...-8GTCGG......,a,,.,
     """
-    return re.sub('\^\S|\$', '', bases)
+    return REOBJ_RM_START_END.sub('', bases)
+    #return re.sub('\^\S|\$', '', bases)
 
 
 def rmIndel(bases):
@@ -38,7 +42,8 @@ def rmIndel(bases):
     ... ...,$.$.$A,..A...,,,.,,............,a,^F,^].^F,
 
     """
-    return re.sub('[-+]\d+[ACGTacgtNn]+', '', bases)
+    return REOBJ_RM_INDEL.sub('', bases)
+    #return re.sub('[-+]\d+[ACGTacgtNn]+', '', bases)
 
 
 def fetch_next(iter_fh):
@@ -61,7 +66,7 @@ def seek_position(target_pos, sample_line, sample_num, sample_tb_iter):
     go_iter_mark = 0  # 1->iterate; 0->donot iterate or hit the end
     if sample_line:
         # chr2    181748  c       2       .,      EA
-        tmp = sample_line.split('\t')
+        tmp = sample_line.split('\t')  ## Must be '\t'
         pos = int(tmp[1])
         if pos == target_pos: # The same position
 
@@ -69,7 +74,7 @@ def seek_position(target_pos, sample_line, sample_num, sample_tb_iter):
             go_iter_mark = 1  # keep iterate
             for i in range(sample_num):
                 try:
-                    if tmp[3*(i + 1)] != '0' and tmp[3*(i+1)+1] != '*':
+                    if tmp[3*(i+1)] != '0' and tmp[3*(i+1)+1] != '*':
                        strand[i], bases[i], quals[i] = best_base(
                             tmp[2], tmp[3*(i+1)+1], tmp[3*(i+1)+2])
 
@@ -79,19 +84,21 @@ def seek_position(target_pos, sample_line, sample_num, sample_tb_iter):
         elif pos < target_pos:
 
             while pos < target_pos:
+
                 sample_line = utils.fetch_next(sample_tb_iter)
                 if sample_line:
-                    tmp = sample_line.split('\t')
+                    tmp = sample_line.split('\t')  ## Must be '\t'
                     pos = int(tmp[1])
                 else:
                     # The end of file. Break the loop.
                     break
 
             if pos == target_pos:
+
                 ref_base = tmp[2]
                 go_iter_mark = 1
                 for i in range(sample_num):
-                    if tmp[3*(i + 1)] != '0' and tmp[3*(i+1)+1] != '*':
+                    if tmp[3*(i+1)] != '0' and tmp[3*(i+1)+1] != '*':
                         strand[i], bases[i], quals[i] = best_base(
                             tmp[2], tmp[3*(i+1)+1], tmp[3*(i+1)+2])
 
@@ -108,7 +115,8 @@ def best_base(ref_base, bases, quality):
     ignore the indels, '^' or '$'
     """
     b = rmIndel(rmStartEnd(bases))
-    idx = np.argmax(quality) # get the best quality index
+    #idx = np.argmax(quality) # get the best quality index
+    idx = 0
 
     ret_base = ref_base if b[idx] in [',', '.'] else b[idx]
 
