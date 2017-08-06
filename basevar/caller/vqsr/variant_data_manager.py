437738703c6271c255c5067f8da54c6a22b0abff
Shujia Huang <huangshujia9@gmail.com>
2017-08-06 14:14:44 +0800

    add VQSR for variant quality control

---
+++ basevar/caller/vqsr/variant_data_manager.py
@@ -0,0 +1,232 @@
+"""
+================================================
+My own Gaussion Mixture Model for SV genotyping.
+Learn form scikit-learn
+================================================
+
+Author: Shujia Huang
+Date  : 2014-01-06 14:33:45
+
+"""
+import sys
+import re
+import os
+import time
+
+import numpy as np
+from sklearn.metrics import roc_curve
+
+from . import variant_recalibrator_argument_collection as VRAC
+from . import variant_datum as vd
+from .. import vcfutils
+
+
+class VariantDataManager(object):
+
+    def __init__(self, data=None):
+        self.VRAC = VRAC.VariantRecalibratorArgumentCollection()
+        self.annotationMean = None
+        self.annotationSTD  = None
+        self.annoTexts      = [['NR', 'Float', 'N ratio of ALT sequence'],
+                               ['HR', 'Integer', 'Homozygous run'],
+                               ['FS', 'Float', 'Phred-scaled p-value using '
+                                'Fisher\'s exact test to detect strand bias']]
+
+        self.data = [] # list <VariantDatum>
+        if data: # data is not None
+            if not isinstance(data[0],vd.VariantDatum): 
+                raise ValueError('[ERROR] The data type should be '
+                                 '"VariantDatum" in VariantDataMa-'
+                                 'nager(),but found %s'% str(type(data[0])))
+            self.data = data
+            for i, d in enumerate(self.data): 
+                self.data[i].annotations = np.array(self.data[i].annotations)
+
+    def SetData(self, data):
+
+        if not isinstance(data[0], vd.VariantDatum): 
+            raise ValueError('[ERROR] The data type should be "VariantDatum" '
+                             'in VariantDataManager(),but found %s' % 
+                             str(type(data[0])))
+        self.data = data
+        for i, d in enumerate(self.data): 
+            self.data[i].annotations = np.array(d.annotations)
+
+    def NormalizeData(self):
+
+        data = np.array([d.annotations for d in self.data], dtype=float)
+        mean = data.mean(axis=0); self.annotationMean = mean
+        std  = data.std(axis=0) ; self.annotationSTD  = std
+
+        # foundZeroVarianceAnnotation
+        if any(std < 1e-5): 
+            raise ValueError('[ERROR] Found annotations with zero variance. '
+                             'They must be excluded before proceeding.')
+        
+        # Each data now is (x - mean)/sd
+        for i, d in enumerate(data):
+            self.data[i].annotations = (d - mean) / std 
+            # trim data by standard deviation threshold and mark failing data 
+            # for exclusion later
+            self.data[i].failingSTDThreshold = False
+            if any(np.abs(self.data[i].annotations) > self.VRAC.STD_THRESHOLD):
+                self.data[i].failingSTDThreshold = True
+
+    def GetTrainingData(self):
+
+        trainingData = [d for d in self.data if ((not d.failingSTDThreshold) 
+                                                 and d.atTrainingSite)]
+        sys.stderr.write(('[INFO] Training with %d variants after standard '
+                          'deviation thresholding.' % len(trainingData)))
+
+        if len(trainingData) < self.VRAC.MIN_NUM_BAD_VARIANTS:
+            sys.stderr.write(('[WARNING] Training with very few variant '
+                              'sites! Please check the model reporting '
+                              'PDF to ensure the quality of the model is '
+                              'reliable.'))
+
+        if len(trainingData) > self.VRAC.MAX_NUM_TRAINING_DATA:
+            sys.stderr.write(('[WARING] Very large training set detected. '
+                              'Downsampling to %d training variants.' %
+                              self.VRAC.MAX_NUM_TRAINING_DATA))
+
+            np.random.shuffle(trainingData) # Random shuffling
+            return list(trainingData[i] 
+                        for i in range(self.VRAC.MAX_NUM_TRAINING_DATA))
+
+        return trainingData 
+
+    def SelectWorstVariants(self, badLod):
+
+        trainingData = []
+        for i,d in enumerate(self.data):
+            if(d.lod < badLod) and (not d.failingSTDThreshold):
+                trainingData.append(d)
+                # I do need: i order to be the same as self.data
+                self.data[i].atAntiTrainingSite = True
+
+        sys.stderr.write('[INFO] Training with worst %d scoring variants '
+                         '--> variants with LOD < %.2f.' % (len(trainingData), badLod))
+
+        if len(trainingData) > self.VRAC.MAX_NUM_TRAINING_DATA:
+            sys.stderr.write('[WARING] Very large training set detected.'
+                             'Downsampling to %d training variants.' %
+                             self.VRAC.MAX_NUM_TRAINING_DATA)
+            np.random.shuffle(trainingData) # Random shuffling
+            return list(trainingData[i] for i in range(self.VRAC.MAX_NUM_TRAINING_DATA))
+
+        return trainingData
+
+    def CalculateWorstLodCutoff(self):
+
+        lodThreshold, lodCum = None, []
+        if len(self.data) > 0:
+
+            lodDist = np.array([[d.atTrainingSite, d.lod] 
+                                for d in self.data if(not d.failingSTDThreshold)])
+
+            # I just use the 'roc_curve' function to calculate the worst 
+            # LOD threshold, not use it to draw ROC curve And 'roc_curve' 
+            # function will output the increse order, so that I don't 
+            # have to sort it again
+            _, tpr, thresholds = roc_curve(lodDist[:,0], lodDist[:,1]) 
+            lodCum = [[thresholds[i], 1.0 - r] for i, r in enumerate(tpr)]
+
+            for i, r in enumerate(tpr):
+                if r > 1.0 - self.VRAC.POSITIVE_TO_NEGATIVE_RATE: 
+                    lodThreshold = round(thresholds[i])
+                    break
+
+        return lodThreshold, np.array(lodCum)
+
+def LoadTrainingSiteFromVCF(vcffile):
+    """
+    Just record the training site positions
+    """
+
+    if vcffile.endseith('.gz'):
+        I = os.popen('gzip -dc %s' % vcffile)
+    else:
+        I = open(vcffile)
+
+    sys.stderr.write('\n[INFO] Loading Training site from VCF', time.asctime())
+    n, dataSet =0, set()
+    for line in I:
+        n += 1
+        if n % 100000 == 0:
+            sys.stderr.write('** Loading lines %d %s' % (n, time.asctime()))
+
+        if re.search(r'^#', line):
+            continue
+
+        col = line.strip().split()
+        dataSet.add(col[0] + ':' + col[1])  # just get the positions
+
+    I.close()
+    sys.stderr.write('[INFO] Finish loading training set %d lines. %s' %
+                     (n, time.asctime()))
+
+    return dataSet
+
+def LoadDataSet(vcfInfile, traningSet, pedFile=None):
+    """
+    Args:
+        'pedFile': The .PED file
+    """
+    # Return a dict: [sample-id] => [parent1, parent2]
+    # if pedFile is None, return {}
+    pedigree = vcfutils.loadPedigree(pedFile)
+
+    if len(traningSet) == 0: 
+        raise ValueError('[ERROR] No Training Data found')
+
+    if vcfInfile.endswith('.gz'):
+        I = os.popen('gzip -dc %s' % vcfInfile)
+    else:
+        I = open(vcfInfile) 
+
+    sys.stderr.write('\n[INFO] Loading data set from VCF', time.asctime())
+    n, data, hInfo = 0, [], vcfutils.Header()
+    for line in I: # VCF format
+        n += 1
+        if n % 100 == 0:
+            sys.stderr.write('** Loading lines %d %s' % (n, time.asctime()))
+
+        col = line.strip().split()
+        if re.search(r'^#CHROM', line):
+            sam2col = {sam: i + 9 for i, sam in enumerate(col[9:])}
+
+        # Record the header information
+        if re.search(r'^#', line):
+            hInfo.record(line.strip())
+            continue
+
+        qual = float(col[5])
+        if qual == 5000 or qual == 10000:
+            # do not use outline quality variants
+            continue
+
+        dp = re.search(r';?CM_DP=([^;]+)', col[7])
+        fs = re.search(r';?FS=([^;]+)', col[7])
+        if not dp or not fs:
+            continue
+
+        fs = round(float(fs.group(1)), 2)
+        dp = round(float(dp.group(1)), 2)
+
+        datum = vd.VariantDatum()
+        datum.raw_annotations = dict(QUAL=qual, DP=dp, FS=fs)
+        datum.annotations = [qual, dp, fs]
+        datum.variantOrder = col[0] + ':' + col[1]
+        if datum.variantOrder in traningSet:
+            datum.atTrainingSite = True
+
+        data.append(datum)
+
+    I.close()
+
+    sys.stderr.write('[INFO] Finish loading data set %d lines. %s' %
+                     (n, time.asctime()))
+
+    return hInfo, np.array(data)
+
---
+++ basevar/caller/vqsr/vqsr.py
@@ -0,0 +1,169 @@
+"""
+=========================================
+Variant quality score recalibrator (VQSR)
+=========================================
+Author: Shujia Huang & Siyang Liu
+Date  : 2014-05-23 11:21:53
+"""
+import re
+import os
+import sys
+import time
+import argparse
+
+from . import variant_data_manager as vdm
+from . import variant_recalibrator as vror
+
+def main(opt):
+
+    # Just record the sites of training data 
+    traningSet = vdm.LoadTrainingSiteFromVCF(opt.trainData)
+
+    # Identify the traning sites
+    hInfo, dataSet = vdm.LoadDataSet(opt.vcfInfile, traningSet, opt.pedfile)
+
+    # init VariantRecalibrator object
+    vr = vror.VariantRecalibrator()
+
+    # Traning modul and calculate the VQ for all dataSet
+    vr.OnTraversalDone(dataSet)
+    vr.VisualizationLodVStrainingSet(opt.figure + '.BadLodSelectInTraining')
+
+    # For Record the Annnotations' values
+    for d in vr.dataManager.annoTexts: 
+        hInfo.add('INFO', d[0], 1, d[1], d[2])
+
+    # Outputting the result as VCF format
+    hInfo.add('INFO', 'VQ', 1, 'Float' , 'Variant Quality')
+    hInfo.add('INFO', 'CU', 1, 'String', 'The annotation which was the worst '
+              'performing in the Gaussian mixture modul, likely the reason why '
+              'the variant was filtered out. It\'s the same tag as <culprit> '
+              'in GATK')
+    hInfo.add('INFO', 'NEGATIVE_TRAIN_SITE', 0, 'Flag', 'This variant was used '
+              'to build the negative training set of bad variants')
+    hInfo.add('INFO', 'POSITIVE_TRAIN_SITE', 0, 'Flag', 'This variant was used '
+              'to build the positive training set of good variants')
+
+    culprit, good, tot = {}, {}, 0.0
+    annoTexts = [d[0] for d in vr.dataManager.annoTexts]
+
+    for k, v in sorted(hInfo.header.items(), key = lambda d: d[0]):
+        print (v)
+
+    if opt.vcfInfile[-3:] == '.gz':
+        I = os.popen('gzip -dc %s' % opt.vcfInfile)
+    else:
+        I = open(opt.vcfInfile)
+
+    sys.stderr.write('\n[INFO] Outputting ...', time.asctime())
+
+    n, j, monitor = 0, 0, True
+    for line in I:
+        n += 1
+        if n % 100000 == 0:
+            sys.stderr.write('** Output lines %d %s' % (n, time.asctime()))
+
+        if re.search(r'^#', line): continue
+        col = line.strip().split()
+
+        # qual = float(col[5])
+        # if qual == 5000 or qual == 10000:
+        #     # do not use outline quality variants
+        #     continue
+
+        dp = re.search(r';?CM_DP=([^;]+)', col[7])
+        fs = re.search(r';?FS=([^;]+)', col[7])
+        if not dp or not fs:
+            continue
+
+        order = col[0] + ':' + col[1]
+        d = dataSet[j]
+        j += 1  # Increase the index of dataSet for the next cycle
+        if d.variantOrder != order:
+            raise ValueError('[BUG] The order(%s) must be the same as '
+                             'dataSet(%s)' % (order, d.variantOrder))
+
+        # Deal with the INFO line
+        vcfinfo = {}
+        for info in col[7].split(';'):
+            k = info.split('=')[0]
+
+            if monitor and k in vcfinfo:
+                monitor = False
+                sys.stderr.write(('[WARNING] The tag: %s double hits in '
+                                  'the INFO column at %s' %
+                                  (k, opt.vcfInfile)))
+            vcfinfo[k] = info
+
+        tot += 1.0  # Record For summary
+        culprit[annoTexts[d.worstAnnotation]] = culprit.get(
+            annoTexts[d.worstAnnotation], 0.0) + 1.0  # For summary
+
+        d.lod = round(d.lod, 2)
+        for lod in [0, 1, 2, 3, 4, 5, 10, 20, 30, 40, 50]:
+            if d.lod >= lod:
+                good[lod] = good.get(lod, 0.0) + 1.0
+
+        if d.atTrainingSite:
+            vcfinfo['POSITIVE_TRAIN_SITE'] = 'POSITIVE_TRAIN_SITE'
+
+        if d.atAntiTrainingSite:
+            vcfinfo['NEGATIVE_TRAIN_SITE'] = 'NEGATIVE_TRAIN_SITE'
+
+        vcfinfo['VQ'] = 'VQ=' + str(d.lod)
+        vcfinfo['CU'] = 'CU=' + annoTexts[d.worstAnnotation]
+
+        for k, v in d.raw_annotations.items():
+            if k not in vcfinfo:
+                vcfinfo[k] = k + '=' + str('%.2f' % v)
+
+        col[7] = ';'.join(sorted(vcfinfo.values()))
+        if d.lod < 0:
+            d.lod = 0  # QUAL: donot allow value below 0
+
+        col[5] = str(d.lod)  # QUAL field should use phred scala
+        print ('\t'.join(col))
+
+    I.close()
+
+    sys.stderr.write('[INFO] Finish Outputting %d lines. %s' % (n, time.asctime()))
+
+    ## Output Summary
+    sys.stderr.write('\n[Summmary] Here is the summary information:\n')
+    for k, v in sorted(good.items(), key = lambda k:k[0]): 
+        sys.stderr.write(('  ** Variant Site score >= %d: %d\t%0.2f' %
+            (k, v, v*100/tot)))
+
+    for k, v in sorted(culprit.items(), key = lambda k:k[0]):
+        sys.stderr.write(('  ** Culprit by %s: %d\t%.2f' %
+            (k, v, v*100.0/tot)))
+
+def cmdopts():
+    """
+    The command line parameters for VQSR 
+    """
+    # usage = ('\nUsage: %prog VQSR [--Train Training data set] '
+    #          '[-i SampleVcfInfile] > Output')
+    optp = argparse.ArgumentParser()
+    optp.add_argument('VQSR')
+
+    optp.add_argument('-i', '--InVcf', dest = 'vcfInfile', metavar = 'VCF',
+                      help = 'VCF for predict.', default = [])
+    optp.add_argument('-T', '--Train', dest = 'trainData', metavar = 'TRU',
+                      help = 'Traing data set at true category', default = [])
+    optp.add_argument('-f', '--fig', dest = 'figure', metavar = 'FIG',
+                      help = 'The prefix of figure.', default = 'figtest')
+
+    opt = optp.parse_args()
+    if len(opt.vcfInfile) == 0:
+        optp.error('Required[-i vcfInfile]\n')
+
+    if len(opt.trainData) == 0:
+        optp.error('Required[-T trainData. VCFFormat]\n')
+
+    sys.stderr.write(('[INFO] Parameters: python', sys.argv[0], 'VQSR',
+                          '\n\t-i', opt.vcfInfile, 
+                          '\n\t-T', opt.trainData, 
+                          '\n\t-f', opt.figure))
+    return opt
+
