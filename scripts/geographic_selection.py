b90895d18ad6086e47b9c7630ce7705cc5e1ede5
Shujia Huang <huangshujia9@gmail.com>
2017-11-14 14:23:51 +0800

    improve the code structrue

--- scripts/geographic_selection.py
+++ scripts/geographic_selection.py
@@ -13,7 +13,6 @@ import argparse
 
 import numpy as np
 
-
 from rpy2 import robjects
 from rpy2 import rinterface
 
@@ -21,6 +20,7 @@ from rpy2 import rinterface
 R_IntVector = robjects.IntVector
 R_MATRIX = robjects.r['matrix']
 R_Fisher_Test = robjects.r['fisher.test']
+
 
 def get_pass_positon(in_file):
 
@@ -31,6 +31,7 @@ def get_pass_positon(in_file):
             sites.add(tok[0]+':'+tok[1])
 
     return sites
+
 
 def get_list_position(in_site_file):
 
@@ -52,6 +53,44 @@ def get_list_position(in_site_file):
             sites[pk] = tok
 
     return sites
+
+
+def load_all_data(in_data_file, pass_sites):
+
+    data = []
+    with gzip.open(in_data_file) \
+            if in_data_file.endswith(".gz") else open(in_data_file) as I:
+
+        for r in I:
+            # chr22  34510633  C  A,G  0.0078,0.564  424.7:8.9:510.1  262.3:7.0:322.2  187.6:3.0:165.4  15.8:0:23.9
+            if r.startswith('#'): continue
+            tok = r.strip().split()
+
+            if len(tok) != 9:
+                sys.stderr.write('[ValueError] %s\n' % '\t'.join(tok))
+                continue
+
+            pos_key = tok[0] + ':' + tok[1]
+            if pos_key not in pass_sites:
+                continue
+
+            alt_base = tok[3].split(',')
+            alt_freq = map(float, tok[4].split(','))
+            centr = map(int, map(float, tok[5].split(':'))) # fist one is REF base
+            north = map(int, map(float, tok[6].split(':'))) # fist one is REF base
+            south = map(int, map(float, tok[7].split(':'))) # fist one is REF base
+
+            for i, (alt_f, alt_b) in enumerate(zip(alt_freq, alt_base)):
+
+                # [pos_key, alele_freq, alt_base, north, central, south]
+                data.append([pos_key,
+                             alt_f,
+                             alt_b.upper(),
+                             str(north[0]) + ':' + str(north[i+1]) + ':' + str(round(float(north[1])/sum(north), 5)) if sum(north)>0 else 'NA',
+                             str(centr[0]) + ':' + str(centr[i+1]) + ':' + str(round(float(centr[1])/sum(centr), 5)) if sum(centr)>0 else 'NA',
+                             str(south[0]) + ':' + str(south[i+1]) + ':' + str(round(float(south[1])/sum(south), 5)) if sum(south)>0 else 'NA'])
+
+    return data
 
 
 def lookup_frequence_nearby_allele(freq, g_idx, nbf_slide_window, data,
@@ -98,18 +137,20 @@ def calculate_significant(nbf_data, have_fisher_test_res):
     for pos_key, f, alt_b, n, c, s in nbf_data:
 
         if pos_key not in have_fisher_test_res:
-            north = map(int, map(float, n.split(':')[:2]))  # first element is REF base
-            centr = map(int, map(float, c.split(':')[:2]))  # first element is REF base
-            south = map(int, map(float, s.split(':')[:2]))  # first element is REF base
+
+            # first value is for REF base in each array blow
+            north = map(int, map(float, n.split(':')[:2]))
+            centr = map(int, map(float, c.split(':')[:2]))
+            south = map(int, map(float, s.split(':')[:2]))
 
             depth = sum(north + centr + south)
             if depth:
-                # User FisherExact to do Fisher exact test for mxn contingency table
+                # Use FisherExact of R for 2x3 contingency table
                 m = R_MATRIX(R_IntVector(north + centr + south), nrow=2)
 
                 try:
-                    # pvalue = R_Fisher_Test(m, workspace=2e6)[0][0]
-                    pvalue = R_Fisher_Test(m, simulate_p_value=True, B=2000)[0][0]
+                    pvalue = R_Fisher_Test(m, workspace=2e6)[0][0]
+                    # pvalue = R_Fisher_Test(m, simulate_p_value=True, B=2000)[0][0]
                     have_fisher_test_res[pos_key] = pvalue
                 except rinterface.RRuntimeError:
                     sys.stderr.write('[SKIP] [workspace not enough(N:C:S)] %s\n' %
@@ -175,58 +216,25 @@ if __name__ == '__main__':
                          sys.argv[0], opt.in_file, opt.num,
                          opt.pass_pos, opt.positions))
 
-    target_sites = get_list_position(opt.positions)
-    pass_sites = get_pass_positon(opt.pass_pos)
-
-    # main function
-    data = []
-    have_fisher_test_res = {}
-    with gzip.open(opt.in_file) \
-            if opt.in_file.endswith(".gz") else open(opt.in_file) as I:
-
-        for r in I:
-            # chr22  34510633  C  A,G  0.0078,0.564  424.7:8.9:510.1  262.3:7.0:322.2  187.6:3.0:165.4  15.8:0:23.9
-            if r.startswith('#'): continue
-            tok = r.strip().split()
-
-            if len(tok) != 9:
-                sys.stderr.write('[ValueError] %s\n' % '\t'.join(tok))
-                continue
-
-            pos_key = tok[0] + ':' + tok[1]
-            if pos_key not in pass_sites:
-                continue
-
-            alt_base = tok[3].split(',')
-            alt_freq = map(float, tok[4].split(','))
-            centr = map(int, map(float, tok[5].split(':'))) # fist one is REF base
-            north = map(int, map(float, tok[6].split(':'))) # fist one is REF base
-            south = map(int, map(float, tok[7].split(':'))) # fist one is REF base
-
-            for i, (alt_f, alt_b) in enumerate(zip(alt_freq, alt_base)):
-
-                # [pos_key, alele_freq, alt_base, north, central, south]
-                data.append([pos_key,
-                             alt_f,
-                             alt_b.upper(),
-                             str(north[0]) + ':' + str(north[i+1]) + ':' + str(round(float(north[1])/sum(north), 5)) if sum(north)>0 else 'NA',
-                             str(centr[0]) + ':' + str(centr[i+1]) + ':' + str(round(float(centr[1])/sum(centr), 5)) if sum(centr)>0 else 'NA',
-                             str(south[0]) + ':' + str(south[i+1]) + ':' + str(round(float(south[1])/sum(south), 5)) if sum(south)>0 else 'NA'])
-
-    # sorted the whole array data from lowest to highest by allele_frequence
-    data.sort(key=lambda x:x[1])
-
     print '\t'.join(['#CHROM', 'POS', 'ID', 'REF', 'ALT', 'DISEASE', 'ALT_freq',
                      'FisherExactTest:p-value', 'Percentile:p-value',
                      'Percentile:Rank','North(REF:ALT:AF)', 'Central(REF:ALT:AF)',
                      'South(REF:ALT:AF)'])
 
+    # load data and sorted the whole array data from lowest to highest by
+    # allele_frequence
+    data = load_all_data(opt.in_file, get_pass_positon(opt.pass_pos))
+    data.sort(key=lambda x:x[1])
+
+    target_sites = get_list_position(opt.positions)
+
     sys.stderr.write('[INFO] ** Data is all loaded .. %s\n' % time.asctime())
 
-    # Now we search all our target position by frequence data
+    # Now we search all the target position in frequence data
     out_res = []
     g_idx = 0
     nbf_slide_window = []
+    have_fisher_test_res = {}
 
     num = 0
     for d in data:
@@ -265,7 +273,6 @@ if __name__ == '__main__':
                      (num, time.asctime()))
 
     out_res.sort(key=lambda x: (x[0], x[1]))
-
     for d in out_res:
         print '\t'.join(map(str, d))
 
