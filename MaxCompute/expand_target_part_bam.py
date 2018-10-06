0a890f5794a4476566c5650133f051bb2b779d65
Tian Li <tianli.tl@alibaba-inc.com>
2018-10-06 17:35:36 +0800

    fix buffer
    Change-Id: I132295c87b412a5b9f9c3ede8f67719c12b515b6

--- MaxCompute/expand_target_part_bam.py
+++ MaxCompute/expand_target_part_bam.py
@@ -2,10 +2,11 @@
 from odps.udf import annotate
 from odps.udf import BaseUDAF
 #from odps.distcache import get_cache_file
+import sys
 
 @annotate('string,string,string,string,string,string,string->string')
 class Expand(BaseUDAF):
-    def __init__(self):
+    def __init__(self, part_idx):
         from odps.distcache import get_cache_file
         res_file = get_cache_file('target_sample.txt')
         self.name_idx = dict()
@@ -16,13 +17,17 @@ class Expand(BaseUDAF):
         res_file.close()
         self.sample_count = i
         self.part_size = 50000
-        self.part_idx = 0
-        self.buffer_size = min(self.sample_count, self.part_size)
+        self.part_idx = part_idx
+        self.buffer_size = max(0, min(self.sample_count - self.part_size * self.part_idx, self.part_size))
+        #print >> sys.stderr, 'Expand init size: %s, part_idx: %s' % (self.buffer_size, self.part_idx)
 
     def new_buffer(self):
+        #print >> sys.stderr, 'new_buffer size: %s, part_idx: %s' % (self.buffer_size, self.part_idx)
         return [''] * self.buffer_size
 
+
     def iterate(self, buffer, sample_name, read_base, read_quality, mapping_quality, read_pos_rank, indel, strand):
+        #print >> sys.stderr, 'iterate'
         idx = self.name_idx.get(sample_name, -1)
         idx_of_part = idx - self.part_size * self.part_idx
         if idx_of_part >= 0 and idx_of_part < self.buffer_size:
@@ -37,30 +42,26 @@ class Expand(BaseUDAF):
                 buffer[i] = s
 
     def terminate(self, buffer):
-        buffer_length = min(self.sample_count - self.part_size * self.part_idx, self.part_size)
-        if buffer_length <= 0:
+        if self.buffer_size <= 0:
             return ''
         for i, s in enumerate(buffer):
             if not s:
                 buffer[i] = 'N\t0\t0\t0\t\t.' # default value
-        return '\t'.join(buffer[0:buffer_length])
+        return '\t'.join(buffer[0:self.buffer_size])
 
 
 @annotate('string,string,string,string,string,string,string->string')
 class Expand_0(Expand):
     def __init__(self):
-        Expand.__init__(self)
-        self.part_idx = 0
+        Expand.__init__(self, 0)
 
 @annotate('string,string,string,string,string,string,string->string')
 class Expand_1(Expand):
     def __init__(self):
-        Expand.__init__(self)
-        self.part_idx = 1
+        Expand.__init__(self, 1)
 
 
 @annotate('string,string,string,string,string,string,string->string')
 class Expand_2(Expand):
     def __init__(self):
-        Expand.__init__(self)
-        self.part_idx = 2
+        Expand.__init__(self, 2)
