bca0fa3f35ef12c16dc6ad0b305ec8c892a75ceb
Shujia Huang <huangshujia9@gmail.com>
2018-10-10 12:16:31 +0800

    ignore base which is not ACGTN in reference

--- basevar/caller/basetypefusionprocess.py
+++ basevar/caller/basetypefusionprocess.py
@@ -178,9 +178,12 @@ class BaseTypeFusionSingleProcess(object):
                         self._out_cvg_file(chrid, position, ref_base, sample_bases,
                                            strands, indels, CVG)
 
-                        # These two lines are just for debug.
+                        # These two lines just for debug.
                         # VCF.write("\t".join([chrid, str(position), ".", ref_base, ".\t.\t.\t.\t."]+[":".join(map(str, [a,b,c])) for a,b,c in zip(sample_bases,sample_base_quals,strands)]))
                         # sys.exit(1)
+
+                        if ref_base.upper not in ['A', 'C', 'G', 'T', 'N']:
+                            continue
 
                         bt = BaseType(ref_base.upper(), sample_bases,
                                       sample_base_quals, cmm=self.cmm)
