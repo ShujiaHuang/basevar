d0122af7c9a8b3949c90ee26ab9d0288d989482c
Shujia Huang <huangshujia9@gmail.com>
2018-11-26 11:45:10 +0800

    commit modify

--- scripts/extract_info.py
+++ scripts/extract_info.py
@@ -3,57 +3,27 @@
 Extract positive and negative training set data
 ================================================
 """
-import os
 import re
 import sys
-
 import time
 
+import pysam
 
-def LoadDataSet(vcfInfile, traningSet):
+
+def LoadDataSet(vcfInfile):
     """
     """
-
-    I = os.popen('gzip -dc %s' % vcfInfile) if vcfInfile.endswith('.gz') else open(vcfInfile)
     sys.stderr.write('\n[INFO] Loading data set from VCF %s\n' % time.asctime())
 
-    n, data = 0, []
-    for line in I: # VCF format
-        n += 1
-        if n % 100 == 0:
-            sys.stderr.write('** Loading lines %d %s\n' % (n, time.asctime()))
+    positive_set = re.search(r';?NEGATIVE_TRAIN_SITE=([^;]+)', col[7])
 
-        # Record the header information
-        if re.search(r'^#', line):
-            continue
+    dp = round(float(dp.group(1)), 2)
 
-        col = line.strip().split()
-        qual = float(col[5])
+    datum.variantOrder = col[0] + ':' + col[1]
+    if datum.variantOrder in traningSet:
+        datum.atTrainingSite = True
 
-        positive_set = re.search(r';?NEGATIVE_TRAIN_SITE=([^;]+)', col[7])
-        negative_set = re.search(r';?POSITIVE_TRAIN_SITE=([^;]+)', col[7])
-
-        if not positive_set or not negative_set:
-            continue
-
-        dp = re.search(r';?CM_DP=([^;]+)', col[7])
-        qu = re.search(r';?QUAL=([^;]+)', col[7])
-        fs = re.search(r';?FS=([^;]+)', col[7])
-
-        if not dp or not fs:
-            continue
-
-        dp = round(float(dp.group(1)), 2)
-        fs = round(float(fs.group(1)), 3)
-
-        if fs >= 10000.0:
-            fs = 10000.0
-
-        datum.variantOrder = col[0] + ':' + col[1]
-        if datum.variantOrder in traningSet:
-            datum.atTrainingSite = True
-
-        data.append(datum)
+    data.append(datum)
 
     I.close()
 
