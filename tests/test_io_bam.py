54d9d85bd60afe0e859a78114b6bf780561dbf1b
Shujia Huang <huangshujia@Shujias-MacBook-Air.local>
2019-05-31 11:15:14 +0800

    test code

---
+++ tests/test_io_bam.py
@@ -0,0 +1,26 @@
+"""Test bam io
+"""
+from basevar.io.bam import get_sample_names
+
+base_dir = "./data/140k_thalassemia_brca_bam"
+bamfile = "./data/140k_thalassemia_brca_bam/bam90.list"
+
+
+def test_get_sample_names(bamfiles):
+    bfs = get_sample_names(bamfiles, False)
+    print(bfs)
+
+    return
+
+
+if __name__ == "__main__":
+
+    print("Startting ......")
+
+    bamfiles = []
+    with open(bamfile) as F:
+        for line in F:
+            bamfiles.append(base_dir + "/" + line.strip())
+
+
+    test_get_sample_names(bamfiles)
---
+++ tests/test_io_fastafile.py
@@ -0,0 +1,34 @@
+"""Test FastaFile
+"""
+from basevar.io.fasta import FastaFile
+
+
+fastafile = "./data/hg19.NC_012920.fasta"
+indexfile = "./data/hg19.NC_012920.fasta.fai"
+
+
+def test_FastaFile(infile):
+    indexfile = infile + ".fai"
+    fa = FastaFile(infile, indexfile)
+
+    # These are the two inner functions which can just be called by Cython code
+    # print("chrM:1 %s" % fa.get_character("chrM", 0))
+    # print("chrM:1-10 %s" % fa.get_sequence("chrM", 0, 10))
+
+    print("chr1 length: %d" % fa.get_reference_length("chr1"))
+    print("chr2 length: %d" % fa.get_reference_length("chr2"))
+    print("chrX length: %d" % fa.get_reference_length("chrX"))
+    print("chrY length: %d" % fa.get_reference_length("chrY"))
+
+    print("The total size of reference is: %d" % fa.get_total_sequence_length())
+    print("fa filename: %s" % fa.filename)
+    print("Number of references seq : %d" % fa.nreferences)
+    print("Tuple of reference seq: ", fa.refnames)
+    print("Tuple of reference length: ", fa.lengths)
+
+    for c, s in zip(fa.refnames, fa.lengths):
+        print("%s length: %d" % (c, s))
+
+
+if __name__ == "__main__":
+    test_FastaFile(fastafile)
