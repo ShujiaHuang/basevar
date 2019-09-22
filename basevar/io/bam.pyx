7a8702adb803d4e6ca5e0c22e7dabd8dd9d5e101
黄树嘉 <huangshujia@Shujia-Huangs-iMac.local>
2019-09-22 22:05:15 +0800

    delete some un-used codes

--- basevar/io/bam.pyx
+++ basevar/io/bam.pyx
@@ -5,10 +5,10 @@ import os
 import sys
 
 from basevar.log import logger
+from basevar.utils cimport c_max
 from basevar.io.read cimport BamReadBuffer
 from basevar.io.htslibWrapper cimport Samfile, ReadIterator, cAlignedRead
 from basevar.caller.batch cimport BatchGenerator
-
 
 cdef bint is_indexable(filename):
     return filename.lower().endswith((".bam", ".cram"))
@@ -74,80 +74,6 @@ cdef list get_sample_names(list bamfiles, bint filename_has_samplename):
     logger.info("Finish loading all %d samples' names\n" % file_num)
     return sample_names
 
-
-# cdef list load_bamdata(dict bamfiles, list samples, bytes chrom, long int start, long int end,
-#                        char* refseq, options):
-#     """
-#     Take a list of BAM files, and a genomic region, and reuturn a list of buffers, containing the
-#     reads for each BAM file in that region.
-#
-#     ``bamfiles`` is a dict of the object of bamfile path for ``samples``(one for each)
-#
-#     Format in ``bamfiles``: {sample: filename}, it could be create by one line code:
-#     bam_objs = {s: f for s, f in zip(samples, input_bamfiles)}
-#
-#     ``samples`` must be the same size as ``bam_objs`` and ``samples`` is the order of input bamfiles
-#
-#     This function could just work for unique sample with only one BAM file. You should merge your
-#     bamfiles first if there are multiple BAM files for one sample.
-#     """
-#
-#     cdef Samfile reader
-#     cdef ReadIterator reader_iter
-#     cdef cAlignedRead *the_read
-#
-#     cdef bint is_compress_read = options.is_compress_read
-#     cdef int qual_bin_size = options.qual_bin_size
-#     cdef int max_read_thd = options.max_reads
-#
-#     cdef int sample_num = len(samples)
-#     cdef BamReadBuffer sample_read_buffer
-#
-#     cdef list population_read_buffers = []
-#
-#     region = "%s:%s-%s" % (chrom, start, end)
-#     cdef int i
-#     for i in range(sample_num):
-#         # assuming the sample is already unique in ``samples``
-#
-#         reader = Samfile(bamfiles[samples[i]])
-#         reader.open("r", True)
-#
-#         # set initial size for BamReadBuffer
-#         sample_read_buffer = BamReadBuffer(chrom, start, end, options)
-#         sample_read_buffer.sample = samples[i]
-#
-#         if (i+1) % 1000 == 0:
-#             logger.info("Loading %d bamfiles in region: %s" % (i+1, region))
-#
-#         try:
-#             reader_iter = reader.fetch(region)
-#         except Exception as e:
-#             logger.warning(e.message)
-#             logger.warning("No data could be retrieved for sample %s in file %s in "
-#                            "region %s" % (samples[i], reader.filename, region))
-#
-#             population_read_buffers.append(sample_read_buffer)
-#             continue
-#
-#         while reader_iter.cnext():
-#
-#             the_read = reader_iter.get(0, NULL)
-#             # if is_compress_read:
-#             #     compress_read(the_read, refseq, start, end, qual_bin_size)
-#             sample_read_buffer.add_read_to_buffer(the_read)
-#             # Todo: we skip all the broken mate reads here, it's that necessary or we should keep them for assembler?
-#
-#         reader.close()
-#
-#         # ``population_read_buffers`` will keep the same order as ``samples``,
-#         # which means will keep the same order as input.
-#         population_read_buffers.append(sample_read_buffer)
-#
-#     # return buffers as the same order of input samples/bamfiles
-#     return population_read_buffers
-
-
 cdef bint load_data_from_bamfile(Samfile bam_reader,
                                  bytes sample_id,
                                  bytes chrom,
@@ -173,9 +99,8 @@ cdef bint load_data_from_bamfile(Samfile bam_reader,
     cdef int total_reads = 0
     cdef BamReadBuffer sample_read_buffer
 
-    cdef long int r_start = max(0, start-1)  # make 0-base
-    cdef long int r_end = max(0, end-1)      # make 0-base
-
+    cdef long int r_start = c_max(0, start-1)  # make 0-base
+    cdef long int r_end = c_max(0, end-1)      # make 0-base
     cdef basestring region = "%s:%s-%s" % (chrom, r_start, r_end)
 
     # set initial size for BamReadBuffer
@@ -192,7 +117,7 @@ cdef bint load_data_from_bamfile(Samfile bam_reader,
         return is_empty
 
     while reader_iter.cnext():
-        # loading data for one sample
+        # loading data for one sample in target region
         the_read = reader_iter.get(0, NULL)
         sample_read_buffer.add_read_to_buffer(the_read)
         total_reads += 1
