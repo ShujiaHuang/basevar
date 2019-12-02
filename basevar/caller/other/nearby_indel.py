8c285d6b5b79ca0a68461ffa1c1a26f77185672a
Shujia Huang <huangshujia9@gmail.com>
2019-12-02 17:16:12 +0800

    add near_by_indel calculation

--- basevar/caller/other/nearby_indel.py
+++ basevar/caller/other/nearby_indel.py
@@ -10,18 +10,17 @@ import sys
 import time
 
 import numpy as np
-import pysam
-
-from .. import utils
-
+from basevar.io.openfile import Open
+from basevar.io.BGZF.tabix import TabixFile
 
 class NearbyIndel(object):
 
-    def __init__(self, in_vcf_file, in_cvg_file, nearby_distance=16):
+    def __init__(self, in_vcf_file, in_cvg_file, output_file, nearby_distance):
 
         self.in_vcf_file = in_vcf_file
-        self.in_cvg_tb = pysam.TabixFile(in_cvg_file)
+        self.in_cvg_tb = TabixFile(in_cvg_file)
         self.nearby_indel_dis = nearby_distance
+        self.output_file_name = output_file
 
     def _close_input_file(self):
         self.in_cvg_tb.close()
@@ -35,10 +34,11 @@ class NearbyIndel(object):
         for r in self.in_cvg_tb.fetch(chr_id, start=start-1, end=end):
             # chrM    30      T       16150   5       8       2       16135   +1C|1,+AA|2   -0.0    7302,8833,4,4
             col = r.strip().split()
-            if col[8] == '.': continue
+            if col[8] == '.':
+                continue
 
             for indel in [s.upper() for s in col[8].split(',')]:
-                indel, n = indel.split(':')
+                indel, n = indel.split('|')
                 n = int(n)
 
                 total_indel_num += n
@@ -49,35 +49,39 @@ class NearbyIndel(object):
 
         # calculate the SDI
         sdi = np.sum([-1.0*(float(v)/total_indel_num) * np.log2(float(v)/total_indel_num)
-                      for k, v in indel_type.iteritems()]) if indel_type else 0.0
+                      for k, v in indel_type.items()]) if indel_type else 0.0
 
         return i_sp, total_indel_num, round(sdi, 3)
 
     def run(self):
 
-        with utils.Open(self.in_vcf_file, 'r') as I:
+        # Final output file
+        if self.output_file_name == "-":
+            OUT = sys.stdout
+        else:
+            OUT = Open(self.output_file_name, 'wb', isbgz=True if self.output_file_name.endswith(".gz") else False)
+
+        with Open(self.in_vcf_file, 'r') as I:
 
             n, monitor = 0, True
             for r in I:
 
                 if r.startswith('##FORMAT=<ID=GT,'):
 
-                    print ('##INFO=<ID=Indel_SDI,Number=1,Type=Float,'
-                           'Description="Indel diversity by Shannon\'s diversity index. '
-                           'The less the better.">')
-                    print ('##INFO=<ID=Indel_SP,Number=1,Type=Integer,'
-                           'Description="Indel species around this position. The less the better.">')
-                    print ('##INFO=<ID=Indel_TOT,Number=1,Type=Integer,'
-                           'Description="Number of Indel around this position. The less the better.">')
+                    OUT.write('##INFO=<ID=Indel_SDI,Number=1,Type=Float,Description="Indel diversity by Shannon\'s '
+                              'diversity index. The less the better.">\n')
+                    OUT.write('##INFO=<ID=Indel_SP,Number=1,Type=Integer,Description="Indel species around this '
+                              'position. The less the better.">\n')
+                    OUT.write('##INFO=<ID=Indel_TOT,Number=1,Type=Integer,Description="Number of Indel around this '
+                              'position. The less the better.">\n')
 
                 if r.startswith('#'):
-                    print (r.strip())
+                    OUT.write(r.strip()+"\n")
                     continue
 
                 n += 1
                 if n % 100000 == 0:
-                    sys.stderr.write('** Output lines %d %s\n' %
-                                     (n, time.asctime()))
+                    sys.stderr.write('** Output lines %d %s\n' % (n, time.asctime()))
 
                 col = r.strip().split()
                 pos = int(col[1])  # Chromsome position
@@ -89,8 +93,7 @@ class NearbyIndel(object):
 
                     if monitor and k in vcfinfo:
                         monitor = False
-                        sys.stderr.write(('[WARNING] The tag: %s double hits in '
-                                          'the INFO column at %s\n' %
+                        sys.stderr.write(('[WARNING] The tag: %s double hits in the INFO column at %s\n' %
                                           (k, self.in_vcf_file)))
                     vcfinfo[k] = info
 
@@ -103,7 +106,7 @@ class NearbyIndel(object):
                 vcfinfo['Indel_TOT'] = 'Indel_TOT=' + str(indel_tot)
 
                 col[7] = ';'.join(sorted(vcfinfo.values()))
-                print ('\t'.join(col))
+                OUT.write("%s\n" % "\t".join(col))
 
         self._close_input_file()
 
