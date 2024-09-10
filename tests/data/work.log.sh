85075c4fc6d9da8e6df5b70848e8fbd9e119fcfd
Shujia Huang <huangshujia9@gmail.com>
2024-09-10 22:07:59 +0800

    first commit

---
+++ tests/data/work.log.sh
@@ -0,0 +1,4 @@
+../../../bin/basevar basetype --mapq=10 --min-af=0.05 --batch-count=1 --thread=1 --regions=CHROMOSOME_I:900-1200 --pop-group=sample_group.info --output-vcf vz.vcf --output-cvg t.cvg -R ce.fa.gz -I range.bam -I range.bam > log2
+
+
+
